41783fd024f47724491906216f72c805295aad6d
Mark Jordan <jordan77@asu.edu>
2025-10-09 09:57:20 -0700

    Cleanup A3 deployment setup

--- basicServers/A3/server.js
+++ basicServers/A3/server.js
@@ -1,20 +1,20 @@
 var express = require("express");
+var path = require("path");
+
 var server = express();
-var bodyParser = require('body-parser');
 var hostname = process.env.HOSTNAME || 'localhost';
-var port = 8080;
+var port = process.env.PORT || 8080;
 
 server.get("/", function (req, res) {
-    res.redirect("index.html")
+    res.redirect("index.html");
 });
 
-
-server.use(bodyParser());
-server.use(express.static(__dirname + '/public'));
+server.use(express.static(path.join(__dirname, 'public')));
 
 console.log("Simple static server listening at http://" + hostname + ":" + port);
 
-//server.listen(port);
-
-if (require.main === module) { server.listen(port); }// Instead do export the app:
-else{ module.exports = server; }
+if (require.main === module) {
+    server.listen(port);
+} else {
+    module.exports = server;
+}
