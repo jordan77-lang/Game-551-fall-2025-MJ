adcc9582974e114128f46651ad098bba2d91da7c
Mark Jordan <jordan77@asu.edu>
2025-10-30 09:27:06 -0700

    move page

---
+++ A5_siteauth/https.js
@@ -0,0 +1,17 @@
+var homeDir = require('path').join(require('os').homedir());
+require('greenlock-express').create({
+  version: 'draft-11'
+, server: 'https://acme-v02.api.letsencrypt.org/directory'
+//, server: 'https://acme-staging-v02.api.letsencrypt.org/directory'  // staging
+, email: 'jordan77@asu.edu'                                     // CHANGE THIS
+, agreeTos: true
+, approveDomains: [ 'jordan77.httpsexample.xyz', 'www.jordan77.httpsexample.xyz' ]              // CHANGE THIS
+, store: require('greenlock-store-fs')
+, configDir: homeDir
+//, app: require('express')().use('/', function (req, res) {
+//    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+//    res.end('Hello, World!\n\n💚 🔒.js');
+//  })
+, app: require('./server.js')
+//, communityMember: true
+}).listen(8080, 8443);
